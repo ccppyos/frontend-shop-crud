ee97a0706753098159c565d35b797fbb348dc0b5
carlos.ramosp <carlos.ramosp@pucp.edu.pe>
2023-01-12 20:38:31 -0500

    backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
-export const backendUrl = "http://127.0.0.1:5000/api";
+//export const backendUrl = "http://127.0.0.1:5000/api";
+export const backendUrl = "https://backend-shop-crud-ccppyos.vercel.app/api";
